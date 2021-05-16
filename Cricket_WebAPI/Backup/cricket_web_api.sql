650da990fdd9491d932cce3c04e6da59d059c71b
Amitoj singh <73503771+amitoj57@users.noreply.github.com>
2021-05-17 09:05:13 +1200

    Add files via upload

---
+++ Cricket_WebAPI/Backup/cricket_web_api.sql
@@ -0,0 +1,16 @@
+SET IDENTITY_INSERT [dbo].[Ratings] ON 
+GO
+INSERT [dbo].[Ratings] ([Id], [PlayerName], [Points], [CountryName]) VALUES (1, N'Ricky Ponting', 450, N'Australia')
+GO
+INSERT [dbo].[Ratings] ([Id], [PlayerName], [Points], [CountryName]) VALUES (2, N'Hershell Gibs', 690, N'South Africa')
+GO
+INSERT [dbo].[Ratings] ([Id], [PlayerName], [Points], [CountryName]) VALUES (3, N'Virat Kohli', 780, N'India')
+GO
+INSERT [dbo].[Ratings] ([Id], [PlayerName], [Points], [CountryName]) VALUES (4, N'Kane Williamsons', 593, N'New Zealand')
+GO
+INSERT [dbo].[Ratings] ([Id], [PlayerName], [Points], [CountryName]) VALUES (5, N'Joe Root', 860, N'England')
+GO
+INSERT [dbo].[Ratings] ([Id], [PlayerName], [Points], [CountryName]) VALUES (6, N'Kumar Sangakara', 800, N'Sri Lanka')
+GO
+SET IDENTITY_INSERT [dbo].[Ratings] OFF
+GO
